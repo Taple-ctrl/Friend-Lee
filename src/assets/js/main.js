53999d43b9fb5449fb9cca28cb69673dba85db0d
Pavel <70695203+Taple-ctrl@users.noreply.github.com>
2023-02-16 18:09:41 +0700

    add UI elements
    add effect write number

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,3 +12,5 @@ document.getElementById("leaseTerm").oninput = function() {
     var value = (this.value-this.min)/(this.max-this.min)*100
     this.style.background = 'linear-gradient(to right, #FF9514 0%, #FF9514 ' + value + '%, #E1E1E1 ' + value + '%, #E1E1E1 100%)'
 };
+
+
